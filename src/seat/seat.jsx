a88b0ad8c650a86c1251fd70e6c3016ff95ed0d9
Uday Sahu <udaysahu982@gmail.com>
2026-01-21 23:03:42 +0530

    comm

--- src/seat/seat.jsx
+++ src/seat/seat.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const seat = () => {
+    
     let seat=[1,2,3,4,5];
+
+    let [selectSeat,setSelectSeat]=useState([]);
+
+    let togleSeat=(seat)=>{
+      setSelectSeat(prev=>
+        prev.includes(seat)
+        ? prev.filter((s)=>s!==seat)
+        : [...prev,seat]
+      );
+    };
+
+    
+
   return (
     <div className='border h-20 flex gap-5 justify-center items-center'>
         hello
-      {seat.map((e,i)=>(<div key={i} className='border h-10 w-10 flex justify-center items-center cursor-pointer bg-red-500'>{e}</div>))}
+        {seat.map((e)=>
+        (<div onClick={()=>togleSeat(e)} key={e}
+         className='border h-10 w-10 flex justify-center items-center cursor-pointer bg-red-500'>{
+          e} </div>))}
 
     </div>
   )
